062258458269d3595b8fa0032fa5b9c950c330b0
RazvanGorb <razvangorb@gmail.com>
2025-02-28 12:45:10 +0200

    Prima Clasa

--- Modul/src/main/java/org/example/NewClass.java
+++ Modul/src/main/java/org/example/NewClass.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class NewClass {
     public static void main(String[] args) {
-        System.out.println("Buna dimiziua!");
+        System.out.println("Buna!");
 
 
     }
